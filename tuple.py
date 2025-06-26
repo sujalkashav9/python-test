04b731da94aea0a6a37b9c14bfb48b06fcd417f4
sujalkashav9 <sujalkashav9@gmail.com>
2025-06-26 15:47:57 +0530

    tuple.py
    simple authantication code

--- tuple.py
+++ tuple.py
@@ -1,16 +1,27 @@
-username = ('jai','sumit','abhinav','sujal','rishi')
-password = (13,69,43,45,65)
+# Tuple of valid usernames
+username = ('jai', 'sumit', 'abhinav', 'sujal', 'rishi')
 
+# Tuple of corresponding passwords (must match index-wise with usernames)
+password = (13, 69, 43, 45, 65)
+
+# Taking input from the user for username
 enter_user = input("Enter your username : ")
+
+# Taking input from the user for password and converting it to integer
 enter_pass = int(input("Enter your pass : "))
+
+# Variable to track if login is successful
 ans = False
-for i in range (len(username)):
-    if(enter_user == username[i] and enter_pass == password[i]):
-        ans = True
-        break
-       
-       
+
+# Looping through the list of usernames
+for i in range(len(username)):
+    # Check if both username and password match at the same index
+    if (enter_user == username[i] and enter_pass == password[i]):
+        ans = True  # If match found, set ans to True
+        break       # Exit the loop since user is authenticated
+
+# Final check to print result based on ans value
 if (ans == True):
-    print("correct")
+    print("correct")  # Login successful
 else:
-    print("wrong")
+    print("wrong")    # Login failed
