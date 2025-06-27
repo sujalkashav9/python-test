f5afaf4597931e00b4bc746ca8d154197e65dd5d
sujalkashav9 <sujalkashav9@gmail.com>
2025-06-27 11:02:19 +0530

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,15 @@
+list = [99,88,77,66,55,]
+print(list)
+
+#using operations of queue in list
+#enquqe
+list.append(44)
+print(list)
+
+#dequeue
+list.remove(88)
+print(list)
+
+#peek
+first_element = list[2]
+print(first_element)
